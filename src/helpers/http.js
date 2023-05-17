9f726b1b88d394e31e3faaec27f378ee825f8661
Solimar <s.huayhua.ma@gmail.com>
2023-05-17 16:30:13 -0500

    removing token

--- src/helpers/http.js
+++ src/helpers/http.js
@@ -1,12 +1,10 @@
 export const http = async (url, method, payload) => {
-  const token = JSON.parse(localStorage.getItem('userInfo'))
   const requestOptions = {
     method,
     headers: {
       'Content-Type': 'application/json',
-      Authorization: token ? 'Bearer ' + token.token : undefined,
     },
-    body: payload ? JSON.stringify({"data":payload}) : undefined,
+    body: payload ? JSON.stringify({ data: payload }) : undefined,
   }
   try {
     const res = await fetch(url, requestOptions)
